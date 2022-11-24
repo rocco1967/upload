e526e447dff785b8e2d8a296f230ba07be283999
rocco1967 <gianfranco.fa@gmail.com>
2022-11-24 12:32:20 +0100

    Add files via upload

---
+++ upload.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 24 10:38:04 2022
+
+@author: 39333
+"""
+import streamlit as st
+from io import StringIO
+import pandas as pd
+#uploaded_files = st.file_uploader("scegli un file csv", 
+                                  #accept_multiple_files=False)
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    #st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    #st.write(stringio)
+
+    # To read file as string:
+    tring_data = stringio.read()
+    #st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
