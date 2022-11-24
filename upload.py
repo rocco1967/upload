88e8af80168380bfb0fd35aabcfaa65b1de22bda
rocco1967 <gianfranco.fa@gmail.com>
2022-11-24 14:12:09 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -27,7 +27,27 @@ if uploaded_file is not None:
     st.write(dataframe)
     st.line_chart(dataframe['charges'])
     
-    
+    from PIL import Image
+
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
+
+...
+
+if choice == "Image":
+		st.subheader("Image")
+		image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+
+		if image_file is not None:
+
+			  # To See details
+			  file_details = {"filename":image_file.name, "filetype":image_file.type,
+                              "filesize":image_file.size}
+			  st.write(file_details)
+
+              # To View Uploaded Image
+			  st.image(load_image(image_file),width=250)
 
     
      
