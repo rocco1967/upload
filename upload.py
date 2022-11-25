dcc38c41e290079a0aa5ec5115717629db2971ae
rocco1967 <gianfranco.fa@gmail.com>
2022-11-25 15:55:51 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -10,7 +10,7 @@ import pandas as pd
 from PIL import Image
 #uploaded_files = st.file_uploader("scegli un file csv", 
                                   #accept_multiple_files=False)
-selectbox =  st.selectbox("large or normal view?",("LARGE", "NORMAL"))
+selectbox =  st.selectbox("large or normal view?",("LARGE"), ("NORMAL")))
 if selectbox == 'LARGE':
    st.set_page_config(layout="wide")
 else:
