3bb47cb87f4771a9d37940403ad242306e92fd0f
rocco1967 <gianfranco.fa@gmail.com>
2022-11-25 15:20:36 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -10,7 +10,7 @@ import pandas as pd
 from PIL import Image
 #uploaded_files = st.file_uploader("scegli un file csv", 
                                   #accept_multiple_files=False)
-screen size= st.selectbox('large or normal view?',('LARGE', 'NORMAL'))
+screen size=  st.selectbox('large or normal view?',('LARGE', 'NORMAL'))
 st.write('You selected:', screen_size)
 selectbox = st.selectbox(
     "How would you like to see ?",
