65fd59d2f9cde12a497cd913fb1b53d0d6a49c6d
rocco1967 <gianfranco.fa@gmail.com>
2022-11-25 11:39:12 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -11,28 +11,34 @@ from PIL import Image
 #uploaded_files = st.file_uploader("scegli un file csv", 
                                   #accept_multiple_files=False)
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
+selectbox = st.selectbox(
+    "How would you like to see ?",
+    ("file_csv", "photo"))
+if selectbox == 'file_csv':
+    uploaded_file = st.file_uploader("Choose a file_csv")
+    if uploaded_file is not None:
     # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
+        bytes_data = uploaded_file.getvalue()
     #st.write(bytes_data)
 
     # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     #st.write(stringio)
 
     # To read file as string:
-    string_data = stringio.read()
+        tring_data = stringio.read()
     #st.write(string_data)
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
-    #st.line_chart(dataframe['charges'])
-    #a=st.input("")    
-    st.line_chart(dataframe['charges'])    
-#file1=st.file_uploader('upload photo')
-#st.image(file1,width=700)
-#option = st.selectbox(
-    #'upload photos ?',
-    #('yes','no'))
-#file1=st.file_uploader('upload photo')
-#st.image(file1,width=700)
+
+    # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
+if selectbox =='photo':
+    uploaded_file = st.file_uploader("Choose a photo")
+    #bytes_data = uploaded_file.getvalue()
+#if uploaded_file is not None:
+    # To read file as bytes:
+    if uploaded_file is not None:
+        st.image(uploaded_file)
+    else:
+        st.write('make your choice')
+#st.image(uploaded_filcached_image=fil.getvalue()
