eb262bfac4e70396d130146291a21326b0866f4d
rocco1967 <gianfranco.fa@gmail.com>
2022-11-28 17:22:38 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -68,7 +68,7 @@ audio=yt.streams.get_audio_only()
     
 st.write(audio)
     #binary_contents=b'audio'
-st.download_button('scegli:'audio)     
+st.download_button('Download binary file'audio)     
         
 #else:
     #st.write('make your choice')
