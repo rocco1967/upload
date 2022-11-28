fa3f2f383fdb92d2822b31ab3dda10b4d84c552b
rocco1967 <gianfranco.fa@gmail.com>
2022-11-28 13:46:12 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -54,13 +54,14 @@ if selectbox =='photo':
         st.write('make your choice')
 #st.image(uploaded_filcached_image=fil.getvalue()
 ############################################################################
-youtube_streams='https://www.youtube.com/watch?v=b7o3F-MVE-Y'
+youtube_streams="ttps://www.youtube.com/watch?v=b7o3F-MVE-Y"
 #youtube_streams=st.text_input('Insert YouTube Link')
 #youtube_streams=youtube_streams.astype(str)
-if youtube_streams is not None:      
+#if youtube_streams is not None:      
     
     #st.write('make your choice')
 
-   yt=YouTube('youtube_streams')
-   audio = yt.streams.get_audio_only()
-st.download_button('download the selected video/audio',audio.download())
+yt=YouTube('youtube_streams')
+audio = yt.streams.get_audio_only()
+audio.download()
+#st.download_button('download the selected video/audio',audio.download())
