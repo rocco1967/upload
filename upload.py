fe669f1df9f5a3109bf66035d9333c6fb8fc995c
rocco1967 <gianfranco.fa@gmail.com>
2022-11-29 11:38:22 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -78,7 +78,7 @@ if youtube_streams is not None:
     audio_file=open(audio,'rb')
     audio_bytes = audio_file.read()
     st.download_button('download_file',data=audio_bytes)
-    st.audio(audio_bytes, format='audio/MP41')
+    st.audio(audio_bytes, format='audio/MP4')
     #audio=BytesIO()
     #audio=audio.getvalue()
     #st.download_button(label='audio',data=audio,)
