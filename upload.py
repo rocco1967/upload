9b9c5c5b5a0fcc1406218a1c043da8f982a42079
rocco1967 <gianfranco.fa@gmail.com>
2022-11-29 11:13:14 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -77,6 +77,7 @@ if youtube_streams is not None:
     #audio=BytesIO(audio)
     audio_file=open(audio,'rb')
     audio_bytes = audio_file.read()
+    st.download_button('download_file',data=audio_bytes)
     st.audio(audio_bytes, format='audio/ogg')
     #audio=BytesIO()
     #audio=audio.getvalue()
