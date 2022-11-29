f0b90a3554bc6df519bd05cdb613882feeb98a3f
rocco1967 <gianfranco.fa@gmail.com>
2022-11-29 09:29:44 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -66,7 +66,7 @@ if youtube_streams is not None:
     #youtube_streams=st.text_input('Insert YouTube Link')
     yt=YouTube(youtube_streams)
     
-    audio=yt.streams.get_audio_only()
+    audio=yt.get_audio_only()
     st.file_uploader(audio)
     #bytes_audio=audio.getvalue()
     #bytes_audio=open(audio,'rb')
