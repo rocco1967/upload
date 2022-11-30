be43481313efe7a8351c4fd384cf85432a965668
rocco1967 <gianfranco.fa@gmail.com>
2022-11-30 15:48:10 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -75,7 +75,7 @@ file_name = 'trial_video.mp4'
 rsp = urllib2.urlopen(link)
 with open(file_name,'wb') as f:
     f.write(rsp.read())
-audio_file=open(rsp,'rb')    
+    f.open(rsp,'rb')    
 ##########################################################################
 import urllib.request #as urllib2
 url = st.text_input("Enter the Youtube")
