5d6403bd74fdbd75a42ca07257273801ff91e50b
rocco1967 <gianfranco.fa@gmail.com>
2022-11-30 10:53:48 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -64,6 +64,8 @@ if selectbox =='photo':
 #youtube_streams='https://www.youtube.com/watch?v=qod03PVTLqk'
 #fp = tempfile.TemporaryFile()
 a=st.text_input('Insert YouTube ')################################
+video_file = open('a', 'rb')
+video_bytes = a.read()
 st.video(a)
 youtube_streams=st.text_input('Insert YouTube Link')#
 var_regex = re.compile(r"^\$*\w+\W")
