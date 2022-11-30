fea4535cb3154cdb61c67848cac93d39252019b7
rocco1967 <gianfranco.fa@gmail.com>
2022-11-30 15:58:09 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -75,8 +75,8 @@ file_name = 'trial_video.mp4'
 rsp = urllib2.urlopen(link)
 with open(file_name,'wb') as f:
      a=f.write(rsp.read())
-a.read()   
-st.download_button('press',data=a)    
+     f.read()   
+     st.download_button('press',data=f)    
     
 ##########################################################################
 import urllib.request #as urllib2
