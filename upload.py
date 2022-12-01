8b53ba331afeb8c02bb3f568515c9eaf75241723
rocco1967 <gianfranco.fa@gmail.com>
2022-12-01 10:31:01 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -64,11 +64,4 @@ if selectbox =='photo':
 from PyPDF2 import PdfReader   #######     PDF
 
 uploaded_file = st.file_uploader("Choose a file_PDF")
-file_path=uploaded_file
-def show_pdf(file_path):
-    with open(file_path,"rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-show_pdf('post1-compressed.pdf')
+sr.write(uploaded_file)
