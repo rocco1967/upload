d24d8a73b71f111b0214b71335cc5fa522d8faa9
rocco1967 <gianfranco.fa@gmail.com>
2023-01-13 14:42:15 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -29,10 +29,10 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days'],
     config['preauthorized']
 )
-name, authentication_status, username = authenticator.login('Login', 'main')
+app, authentication_status, username = authenticator.login('Login', 'main')
 if authentication_status:
     authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{name}*')
+    st.write(f'Welcome *{app}*')
     st.title('Some content')
 elif authentication_status == False:
     st.error('Username/password is incorrect')
