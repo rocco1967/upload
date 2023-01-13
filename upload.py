7c87676ad4d29e901f0dbadfeb3aeb4527061506
rocco1967 <gianfranco.fa@gmail.com>
2023-01-13 14:13:13 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -29,7 +29,7 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days'],
     config['preauthorized']
 )
-name, authentication_status, username = authenticator.login('Login', 'main')
+GF, authentication_status, username = authenticator.login('Login', 'main')
 if authentication_status:
     authenticator.logout('Logout', 'main')
     st.write(f'Welcome *{name}*')
@@ -38,7 +38,14 @@ elif authentication_status == False:
     st.error('Username/password is incorrect')
 elif authentication_status == None:
     st.warning('Please enter your username and password')
-    
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')    
 
 ###################################################################
 #st.write(pytube.__version__)
