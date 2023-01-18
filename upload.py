c35969df5ebd974c9db6a4a8d737a03d0378741b
rocco1967 <gianfranco.fa@gmail.com>
2023-01-18 13:33:02 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -85,4 +85,4 @@ with open(os.path.join("/tmp", files.name), "wb") as f:
     f.write(bytes_data)  # write this content elsewhere
 with open(os.path.join("/tmp",a),"rb") as r:
     #st.audio(r, format='audio/wav')
-    st.download_button('DOWNLOAD MUSIC_FILE',data=r+str(a))
+    st.download_button('DOWNLOAD MUSIC_FILE',data=r)
