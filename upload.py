c39ab2fc1f28323c30a9827d4c3adfd9f11f4689
rocco1967 <gianfranco.fa@gmail.com>
2023-01-19 15:13:27 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -109,4 +109,4 @@ def youtube2mp3 (url,outdir):
     else:
         st.write(f'ERROR: {yt.title}could not be downloaded!')
 
-youtube2mp3=(st.file_uploader("please enter youtube video url:"),st.download_button('download music'))
+youtube2mp3=st.file_uploader("please enter youtube video url:")#,st.download_button('download music'))
