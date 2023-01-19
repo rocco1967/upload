67cc7a863d3162aa9cb430decbbadce9dea224c2
rocco1967 <gianfranco.fa@gmail.com>
2023-01-19 15:24:23 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -86,13 +86,13 @@ with open(os.path.join("/tmp", files.name), "wb") as f:
 with open(os.path.join("/tmp",a),"rb") as r:
     #st.audio(r, format='audio/wav')
     st.download_button('DOWNLOAD MUSIC_FILE',data=r,file_name=a)
-#######################################################################################################################################
+
 from pytube import YouTube
 import os
 from pathlib import Path
 
 
 
-#a=st.file_uploader("please enter youtube video url:")
+a=st.file_uploader("please enter youtube video url:")
 #,st.download_button('download music'))
 
