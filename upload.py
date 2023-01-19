ab656bf69aca69e270c8eea2a6ec2a111777e0b5
rocco1967 <gianfranco.fa@gmail.com>
2023-01-19 16:13:52 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -67,10 +67,9 @@ if selectbox =='photo':
         st.write('make your choice')
 #st.image(uploaded_filcached_image=fil.getvalue()
 ###############################################################################################################################
-import urllib2
-from urllib import request
+import requests
 URL = st.text_input('url video')
-video_file = urllib.urlopen('URL')
+video_file = requests.get(URL)
 
 ####################################################################################################################################
 
